55ec744e69a0b34ea8ba63bdf1d9bbd9c575cda4
Kyle Maxwell <kmaxwell@verisign.com>
2016-10-05 14:28:49 -0500

    Add type assertion test

--- tests/TRX_tests.py
+++ tests/TRX_tests.py
@@ -1,11 +1,14 @@
-from nose.tools import *
-import TRX 
+from TRX import TRX
+
 
 def setup():
     print "SETUP!"
 
+
 def teardown():
     print "TEAR DOWN!"
 
-def test_basic():
-    print "I RAN!"
+
+def test_entity_creation():
+    entity = TRX.MaltegoEntity()
+    assert isinstance(entity, TRX.MaltegoEntity)
